fb749902b212b570f582c19c2a10e215534c48ec
Sanyam Pandey <sanyam071104@gmail.com>
2024-12-30 18:39:41 +0530

    So this is I am doing

---
+++ src/CursorOverlay/CursorOverlay.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import cursor from "../resources/icons/selection.svg";
+
+const CursorOverlay = () => {
+  const cursors = useSelector((state) => state.cursor.cursors);
+
+  return (
+    <>
+      {cursors.map((c) => (
+        <img
+          key={c.userId}
+          alt="Nothing"
+          className="cursor"
+          style={{ position: "absolute", left: c.x, top: c.y, width: "30px" }}
+          src={cursor}
+        />
+      ))}
+    </>
+  );
+};
+
+export default CursorOverlay;
